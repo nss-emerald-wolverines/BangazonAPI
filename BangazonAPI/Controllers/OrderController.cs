9cdcef3086521f2841d0e73a3181edfc82a9c760
Colleen Woolsey <rlwool@aol.com>
2019-04-02 15:54:47 -0500

    Stashing unfinished controller

---
+++ BangazonAPI/Controllers/OrderController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public OrderController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // CODE FOR GETTING A LIST - GET: api/Order
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id,
+                                           CustomerId,
+                                           PaymentTypeId,
+                                        FROM Order";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Order> orders = new List<Order>();
+
+                    while (reader.Read())
+                    {
+                        Order order = new Order
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
+                            PaymentTypeId = reader.GetInt32(reader.GetOrdinal("PaymentTypeId"))
+                        };
+
+                        orders.Add(order);
+                    }
+                    reader.Close();
+
+                    return Ok(orders);
+                }
+            }
+        }
+
+
+        // GET: api/Order/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Order
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Order/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- BangazonAPI/Models/Order.cs
+++ BangazonAPI/Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,18 @@ namespace BangazonAPI.Models
 {
     public class Order
     {
+        public int Id { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public int PaymentTypeId { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public PaymentType PaymentType { get; set; }
+
+        // public List<Exercise> Exercises { get; set; } = new List<Exercise>();
     }
 }
