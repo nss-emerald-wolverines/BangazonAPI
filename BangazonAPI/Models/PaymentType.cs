e199c90940344816db69fbc1e3ee68329e489473
Zac Crawford <zwcrawford@gmail.com>
2019-04-02 14:40:25 -0500

    pushing up PaymentType model

--- BangazonAPI/Models/PaymentType.cs
+++ BangazonAPI/Models/PaymentType.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+// Making the basic class with no constructor at the moment.
 namespace BangazonAPI.Models
 {
     public class PaymentType
