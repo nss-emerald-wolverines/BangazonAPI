47e862188f621978c1d51038608ccafed6210fb8
unknown <dakota.cookson1995@gmail.com>
2019-04-03 12:22:06 -0500

    completed customer controler and testing

--- BangazonAPI/Controllers/CustomerController.cs
+++ BangazonAPI/Controllers/CustomerController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using BangazonAPI.Models;
 namespace BangazonAPI.Controllers
 
 {
--- TestBangazonAPI/BangazonAPItTests.cs
+++ TestBangazonAPI/BangazonAPItTests.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using BangazonAPI;
-
+using BangazonAPI.Models;
 namespace BangazonAPItTests
 {
     public class CustomerTests
